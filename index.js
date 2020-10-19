ff9d7d15e8b4b2550b1b9c22af7d59606fa15a06
unknown <vicky.electron831@gmail.com>
2020-10-20 03:56:25 +0530

    added new

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-var express = require("express");
-const app = express();
-app.get("/",(req,res)=>{
+var http = require('http');
 
-    res.json({
-"success":"this is homepagge"
-    });
-});
-app.listen(5000);
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(process.env.PORT || 5000);
